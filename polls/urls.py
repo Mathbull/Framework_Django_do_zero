0f9ab2a5082fb534840519bd170a740a5cee4e96
Matheus Santos <santos.matheus1012@gmail.com>
2024-11-18 11:53:00 -0300

    Criado o forms para a criação de perguntas, usando viws baseada em função class

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from polls.views import index, ola
+from polls.views import index, ola, QuestionCreateView, question_create
 
 urlpatterns = [
     path('index/', index, name='index'),
 
-    path('ola/', ola, name='ola')
+    path('ola/', ola, name='ola'),
+
+    # Criando as rodas:
+    # Baseada em classe
+    path('enquete/add', QuestionCreateView.as_view(), name="poll_add"),
+    #baseada em função
+    path('pergunta/create', question_create, name="poll_create"),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 from polls.models import Question
+from polls.forms import QuestionForm
 
 # define um view baseado em função
 def index(request):
@@ -24,5 +29,28 @@ def ola(request):
     question = Question.objects.all()
     context = {'all_question': question}
     return render(request, 'polls/question.html', context)
-    
-    
+
+
+class QuestionCreateView(LoginRequiredMixin,CreateView):
+    model = Question
+    template_name = "polls/question_form.html"
+    fields = ("question_text", "pub_date")
+    success_url = reverse_lazy("index")
+    success_message = "Pergunta criada com sucesso..."
+
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super(QuestionCreateView, self).form_valid(form)    
+
+@login_required
+def question_create(request):
+    context = {}
+    form = QuestionForm(request.POST or None, request.FILES or None)
+    context['form'] =form
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Pergunta criada com sucesso!')
+            return redirect('index')
+    return render(request, 'polls/question_form.html', context)
